7b1b6a4ca2e6be44897096f96e9e383c92a5a7fb
Ash digit <ashdigit44@gmail.com>
2025-01-04 13:46:27 +0000

    the colour flipper is done

--- colour-flipper/src/App.jsx
+++ colour-flipper/src/App.jsx
@@ -2,27 +2,26 @@ import { useState, StyleSheet} from 'react'
 import './App.css'
 
 function App() {
-  const [cName, setCName] = useState("white")
+  const [cName, setCName] = useState("bg black")
   const [count, setCount] = useState(0)
 
   const colourPalet = ['red', 'blue', 'green']
   const myFunc = function(){
     
     setCount(count + 1);
-    setCName(colourPalet[count])
+    setCName(`bg ` + colourPalet[count])
     if(count >= colourPalet.length-1){
       setCount(0)
     }
-    // if(colourNumber > colourNumber.length){
-    // console.log(colourNumber, '<---colourNumber before')
-
-    //   colourNumber = 0;
-       
-    // }
-    console.log(cName, count)
   }
 
-  return (<button className={cName}  onClick={myFunc}>Click to flip the colour</button>)
+  return (
+  <>
+    <div className={cName}></div>
+  <button className='btn' onClick={myFunc}>Click to flip the colour</button>
+  </>
+
+)
 }
 
 export default App
